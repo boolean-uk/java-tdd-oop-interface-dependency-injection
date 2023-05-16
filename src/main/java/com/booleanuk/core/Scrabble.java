be807643bd0bc924f437d70cece2132d704c394d
Rose <rosevandergiessen@proton.me>
2023-05-16 11:20:32 +0200

    created alphabet interface & greek, russian classes

--- src/main/java/com/booleanuk/core/Scrabble.java
+++ src/main/java/com/booleanuk/core/Scrabble.java
@@ -5,9 +5,8 @@ import java.util.Map;
 public class Scrabble {
     Map<Character, Integer> letterScores;
 
-    public Scrabble() {
-        Alphabet a = new Alphabet();
-        this.letterScores = a.getLetterScores();
+    public Scrabble(Alphabet alphabet) {
+        this.letterScores = alphabet.getLetterScores();
     }
 
     public int score(String word) {
@@ -19,7 +18,6 @@ public class Scrabble {
                 total += score;
             }
         }
-
         return total;
     }
 }
