480609cf20063210a09264f76e61170b89806c95
Enock97 <enockladu@hotmail.com>
2024-01-22 09:54:09 +0100

    Completed exercise, although I can't run the tests due to "unmappable character" errors

--- src/main/java/com/booleanuk/core/Scrabble.java
+++ src/main/java/com/booleanuk/core/Scrabble.java
@@ -3,19 +3,51 @@ package com.booleanuk.core;
 import java.util.Map;
 
 public class Scrabble {
-    Map<Character, Integer> letterScores;
+    Map<Character, Integer> englishLetterScores;
+    Map<Character, Integer> russianLetterScores;
+    Map<Character, Integer> greekLetterScores;
 
     public Scrabble() {
-        Alphabet a = new Alphabet();
-        this.letterScores = a.getLetterScores();
+        Alphabet englishAlphabet = new EnglishAlphabet();
+        Alphabet russianAlphabet = new RussianAlphabet();
+        Alphabet greekAlphabet = new GreekAlphabet();
+        this.englishLetterScores = englishAlphabet.getLetterScores();
+        this.russianLetterScores = russianAlphabet.getLetterScores();
+        this.greekLetterScores = greekAlphabet.getLetterScores();
     }
 
-    public int score(String word) {
+    public int englishScore(String word) {
         int total = 0;
 
         for (char ch : word.toCharArray()) {
-            if (this.letterScores.containsKey(ch)) {
-                int score = this.letterScores.get(ch);
+            if (this.englishLetterScores.containsKey(ch)) {
+                int score = this.englishLetterScores.get(ch);
+                total += score;
+            }
+        }
+
+        return total;
+    }
+
+    public int russianScore(String word) {
+        int total = 0;
+
+        for (char ch : word.toCharArray()) {
+            if (this.russianLetterScores.containsKey(ch)) {
+                int score = this.russianLetterScores.get(ch);
+                total += score;
+            }
+        }
+
+        return total;
+    }
+
+    public int greekScore(String word) {
+        int total = 0;
+
+        for (char ch : word.toCharArray()) {
+            if (this.greekLetterScores.containsKey(ch)) {
+                int score = this.greekLetterScores.get(ch);
                 total += score;
             }
         }
