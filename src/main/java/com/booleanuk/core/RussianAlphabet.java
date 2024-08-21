373bd9b2f5d78462a4cb450db45c72450c8f629c
Dag Andreas <daanfofo@gmail.com>
2024-08-21 10:07:31 +0200

    added russian alphabet

---
+++ src/main/java/com/booleanuk/core/RussianAlphabet.java
@@ -0,0 +1,47 @@
+package com.booleanuk.core;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RussianAlphabet implements Alphabet{
+	public Map<Character, Integer> getLetterScores() {
+		HashMap<Character, Integer> russianAlphabet = new HashMap<>();
+		try{
+			File f = new File("RUSSIAN_LETTER_SCORES.md");
+			Scanner sc = new Scanner(f);
+			sc.nextLine(); sc.nextLine();
+			while (sc.hasNext()){
+				String line = sc.nextLine();
+				char splitChar = line.charAt(0);
+				String splitstr = String.valueOf(splitChar);
+				line = line.substring(1, line.length()-1).replaceAll("\\s", "");
+				int splitindex = line.indexOf(splitChar);
+				String lts  = line.substring(0, splitindex);
+				String val = line.substring(splitindex+1, line.length());
+				String[] letters = lts.split(",");
+				int value = Integer.valueOf(val);
+
+				for(String str: letters){
+					russianAlphabet.put(str.charAt(0), value);
+				}
+
+
+				System.out.println(letters);
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println(russianAlphabet);
+		return russianAlphabet;
+	}
+
+
+	public static void main(String[] args) {
+		RussianAlphabet ra = new RussianAlphabet();
+		ra.getLetterScores();
+	}
+}
