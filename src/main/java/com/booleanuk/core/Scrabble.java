b887fc689faf6f95e85eed0cdb23f4387269b058
Thomas Kristiansen <thomaskri0801@hotmail.com>
2025-01-13 11:32:09 +0100

    Core tasks done

--- src/main/java/com/booleanuk/core/Scrabble.java
+++ src/main/java/com/booleanuk/core/Scrabble.java
@@ -3,19 +3,51 @@ package com.booleanuk.core;
 import java.util.Map;
 
 public class Scrabble {
-    Map<Character, Integer> letterScores;
+    Map<Character, Integer> letterScoresEnglish;
+    Map<Character, Integer> letterScoresGreek;
+    Map<Character, Integer> letterScoresRussian;
 
     public Scrabble() {
-        Alphabet a = new Alphabet();
-        this.letterScores = a.getLetterScores();
+        Alphabet a = new EnglishAlphabet();
+        Alphabet greekAlphabet = new GreekAlphabet();
+        Alphabet russianAlphabet = new RussianAlphabet();
+        this.letterScoresEnglish = a.getLetterScores();
+        this.letterScoresGreek = greekAlphabet.getLetterScores();
+        this.letterScoresRussian = russianAlphabet.getLetterScores();
     }
 
-    public int score(String word) {
+    public int scoreEnglish(String word) {
         int total = 0;
 
         for (char ch : word.toCharArray()) {
-            if (this.letterScores.containsKey(ch)) {
-                int score = this.letterScores.get(ch);
+            if (this.letterScoresEnglish.containsKey(ch)) {
+                int score = this.letterScoresEnglish.get(ch);
+                total += score;
+            }
+        }
+
+        return total;
+    }
+
+    public int scoreGreek(String word) {
+        int total = 0;
+
+        for (char ch : word.toCharArray()) {
+            if (this.letterScoresGreek.containsKey(ch)) {
+                int score = this.letterScoresGreek.get(ch);
+                total += score;
+            }
+        }
+
+        return total;
+    }
+
+    public int scoreRussian(String word) {
+        int total = 0;
+
+        for (char ch : word.toCharArray()) {
+            if (this.letterScoresRussian.containsKey(ch)) {
+                int score = this.letterScoresRussian.get(ch);
                 total += score;
             }
         }
