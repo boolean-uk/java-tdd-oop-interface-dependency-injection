51d37cb5544980aac033af51278338d499ad6c54
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-01-13 11:32:01 +0100

    Wilmer Winkler

--- src/main/java/com/booleanuk/core/EngAlphabet.java
+++ src/main/java/com/booleanuk/core/EngAlphabet.java
@@ -3,7 +3,7 @@ package com.booleanuk.core;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Alphabet {
+public class EngAlphabet implements IAlphabet{
     public Map<Character, Integer> getLetterScores() {
         return new HashMap<>(){{
             put('a', 1);
---
+++ src/main/java/com/booleanuk/core/IAlphabet.java
@@ -0,0 +1,11 @@
+package com.booleanuk.core;
+
+import java.util.Map;
+
+public interface IAlphabet {
+
+     Map<Character, Integer> getLetterScores();
+
+
+
+}
